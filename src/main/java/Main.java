2a5504e9f00cefc468805bfb75618a78d9024906
manasthorat7 <manasthorat7@gmail.com>
2026-06-16 19:29:55 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,49 +5,61 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         Scanner sc = new Scanner(System.in);
-        while(true){
-        System.out.print("$ ");
-        
-        String s = sc.nextLine();
-        if(s.equals("exit")){
-            break;
-        }
-        else if(s.startsWith("echo")){
-            System.out.println(s.substring(5));
-        }
-        else if(s.startsWith("type")){
-            String command = s.substring(5);
-            if(command.equals("echo") || command.equals("type") || command.equals("exit")){
-                System.out.println(command + " is a shell builtin");
-            }
-            else{
-                    
+        while (true) {
+            System.out.print("$ ");
+
+            String s = sc.nextLine();
+            if (s.equals("exit")) {
+                break;
+            } else if (s.startsWith("echo")) {
+                System.out.println(s.substring(5));
+            } else if (s.startsWith("type")) {
+                String command = s.substring(5);
+
+                if (command.equals("echo") || command.equals("type") || command.equals("exit")) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    boolean found = false;
+
+                    for (String dir : System.getenv("PATH").split(File.pathSeparator)) {
+                        File f = new File(dir, command);
+
+                        if (f.exists() && f.canExecute()) {
+                            System.out.println(command + " is " + f.getAbsolutePath());
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println(command + ": not found");
+                    }
+                }
+            } else {
                 String[] parts = s.split(" ");
                 String cmd = parts[0];
 
                 boolean found = false;
 
-                    for(String dir : System.getenv("PATH").split(File.pathSeparator)){
-                        File f = new File(dir, cmd);
+                for (String dir : System.getenv("PATH").split(File.pathSeparator)) {
+                    File f = new File(dir, cmd);
 
-                         if(f.exists() && f.canExecute()){
-                            parts[0] = f.getAbsolutePath();
+                    if (f.exists() && f.canExecute()) {
+                        parts[0] = f.getAbsolutePath();
 
-                            ProcessBuilder pb = new ProcessBuilder(parts);
-                            pb.inheritIO();
-                            pb.start().waitFor();
-                            found = true;
-                            break;
-                        }
-                   }
-                    if(!found){
-                     System.out.println(cmd + ": not found");
+                        ProcessBuilder pb = new ProcessBuilder(parts);
+                        pb.inheritIO();
+                        pb.start().waitFor();
+
+                        found = true;
+                        break;
                     }
-               }
+                }
+
+                if (!found) {
+                    System.out.println(cmd + ": command not found");
+                }
             }
-        else{
-            System.out.println(s + ":" + " " + "command not found");
-        }
         }
     }
 }
