60f86212515027bfb5f166d48ad45f8e38165ca6
manasthorat7 <manasthorat7@gmail.com>
2026-06-16 19:32:07 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
         Scanner sc = new Scanner(System.in);
+
         while (true) {
             System.out.print("$ ");
 
             String s = sc.nextLine();
+
             if (s.equals("exit")) {
                 break;
-            } else if (s.startsWith("echo")) {
+            }
+
+            else if (s.startsWith("echo ")) {
                 System.out.println(s.substring(5));
-            } else if (s.startsWith("type")) {
+            }
+
+            else if (s.startsWith("type ")) {
                 String command = s.substring(5);
 
-                if (command.equals("echo") || command.equals("type") || command.equals("exit")) {
+                if (command.equals("echo")
+                        || command.equals("type")
+                        || command.equals("exit")) {
                     System.out.println(command + " is a shell builtin");
                 } else {
                     boolean found = false;
@@ -35,7 +42,9 @@ public class Main {
                         System.out.println(command + ": not found");
                     }
                 }
-            } else {
+            }
+
+            else {
                 String[] parts = s.split(" ");
                 String cmd = parts[0];
 
@@ -45,8 +54,6 @@ public class Main {
                     File f = new File(dir, cmd);
 
                     if (f.exists() && f.canExecute()) {
-                        parts[0] = f.getAbsolutePath();
-
                         ProcessBuilder pb = new ProcessBuilder(parts);
                         pb.inheritIO();
                         pb.start().waitFor();
@@ -61,5 +68,7 @@ public class Main {
                 }
             }
         }
+
+        sc.close();
     }
 }
